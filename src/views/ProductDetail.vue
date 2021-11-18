47cb78c62de9becccda65a440de6013c66bb3826
unknown <thatchanon3232@gmail.com>
2021-11-18 23:52:57 +0700

    [ADD] footer

--- src/views/ProductDetail.vue
+++ src/views/ProductDetail.vue
@@ -107,11 +107,6 @@
             >
               <MoreProduct></MoreProduct>
             </div>
-            <!-- <div>
-              <div
-                class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4"
-              ></div>
-            </div> -->
           </main>
         </div>
       </div>
